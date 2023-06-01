fe3afc9380eb4f676070d439e68d273fa34e517a
Rubies21 <jadesolaadeagbo@gmail.com>
2023-06-01 21:37:12 +0100

    Progress commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full flex flex-col ">
-        <div class="w-full lg:h-screen bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/ceb3b1eb-2673-4dd9-a6e3-0cd7a5e130ee/8b9932b0-a2a9-4226-b129-5dd7c8eba66a/NG-en-20230522-popsignuptwoweeks-perspective_alpha_website_medium.jpg')] bg-cover md:bg-auto bg-center border-b-3 border-solid border-red-500">
+        <div class="w-full lg:h-screen bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/ceb3b1eb-2673-4dd9-a6e3-0cd7a5e130ee/8b9932b0-a2a9-4226-b129-5dd7c8eba66a/NG-en-20230522-popsignuptwoweeks-perspective_alpha_website_medium.jpg')] bg-cover md:bg-auto bg-center">
             <div class="w-full h-full bg-black/60 ">
                 <div class="lg:px-40 py-5 flex justify-between items-center px-10">
                     <h1 class="text-customRed lg:text-5xl text-2xl sm:text-4xl font-bold">NETFLIX</h1>
@@ -31,8 +31,8 @@
             </div>       
         </div>  
 
-        <div class="border-t-8 border-gray-700/50 bg-black text-white">
-            <div class="flex lg:flex-row justify-between items-center pt-20 lg:px-36 px-10 pb-16 border-b-8 border-gray-700/50 flex-col">
+        <div class="border-t-8 border-customGray bg-black text-white">
+            <div class="flex lg:flex-row justify-between items-center pt-20 lg:px-36 px-10 pb-16 border-b-8 border-customGray flex-col">
                 <span class="text-center lg:text-start w-full lg:w-1/2">
                     <h1 class="lg:text-5xl text-3xl font-extrabold">Enjoy on your TV</h1>
                     <p class="lg:text-2xl text-lg lg:font-medium pt-5 w-full">Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</p>                    
@@ -41,7 +41,7 @@
                 <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" alt="">
             </div>
             
-            <div class="flex justify-between items-center pt-12 lg:pl-36 lg:pr-12 pb-12 border-b-8 border-gray-700/50 lg:flex-row flex-col-reverse">
+            <div class="flex justify-between items-center pt-12 lg:pl-36 lg:pr-12 pb-12 border-b-8 border-customGray lg:flex-row flex-col-reverse">
                 <img class="lg:w-1/2" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" alt=""/>
 
                 <span class="lg:pr-36 lg:text-start text-center">
@@ -50,7 +50,7 @@
                 </span>
             </div>
 
-            <div class="flex lg:justify-start justify-center lg:pt-20 pt-16 lg:px-40 lg:pb-12 pb-28 border-b-8 border-gray-700/50 w-full">
+            <div class="flex lg:justify-start justify-center lg:pt-20 pt-16 lg:px-40 lg:pb-12 pb-28 border-b-8 border-customGray w-full">
                 <span class="lg:w-1/2 w-full">
                     <h1 class="lg:text-5xl text-3xl font-bold lg:w-1/2 text-center lg:text-start">Watch everywhere</h1>
                     <p class="lg:text-2xl text-xl lg:w-3/4 lg:pt-8 pt-4 text-center lg:px-0 px-8 lg:text-start">Stream unlimited movies and TV shows on your phone, tablet, laptop and TV.</p>                    
@@ -58,7 +58,7 @@
 
             </div>
 
-            <div class="flex justify-between items-center pt-12 lg:pl-36 lg:pr-12 pb-12 border-b-8 border-gray-700/50 lg:flex-row flex-col-reverse">
+            <div class="flex justify-between items-center pt-12 lg:pl-36 lg:pr-12 pb-12 border-b-8 border-customGray lg:flex-row flex-col-reverse">
                 <img src="https://occ-0-299-1168.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABRFH-vdJSR0qkt72Fgqx45jsKfXao7Ds3YDvdyrPvl7kJHMxfm-9aKjlb4EY3ezBGDQdt0WhJOxOOeMcvDfgTw_cg-XWV8mt8AUk.png?r=b36" alt="">
 
                 <span class="lg:pr-20 lg:text-start text-center">
@@ -67,20 +67,65 @@
                 </span>
             </div>
             
-            <div>
+            <div class=" ">
                 <h1 class="lg:text-5xl text-3xl font-bold flex justify-center pt-16">Frequently Asked Questions</h1>
 
                 <div class="lg:ml-40 ml-10">
                         <AccordionComponent/>
                 </div>
 
-
-                <!-- <div class="grid place-items-center">
-                  <AccordionComponent/>
-                </div> -->
-
-
             </div>
+
+            <div class="pt-10 sm:px-0 px-10 border-b-8 border-customGray">
+                <p class="text-center text-xl ">Ready to watch? Enter your email to create or restart your membership.</p>
+
+                <div class="flex justify-center p-5">
+                    <form class="flex items-center w-full justify-center sm:flex-row flex-col gap-2 pb-10">
+                        
+                        <input class="w-3/4 lg:w-4/12 sm:w-1/2 bg-black/50 sm:h-14 h-14 pl-4 text-lg border border-solid rounded bg-gray-800 text-gray-100 border-green-600 ::placeholder text-sm pb-6" placeholder="Email address" type="email" name="email"/>
+                        
+
+                        <button class="bg-customRed sm:text-2xl text-xl px-5 rounded flex items-center sm:h-full h-12 gap-2 font-medium">Get Started<i class="fa-solid fa-chevron-right"></i></button>
+                    </form>                            
+                </div>
+            </div>
+
+
+            <div class="pl-10 pt-5 md:pl-36 md:pt-16 text-md pb-20">
+                <a href="https://help.netflix.com/contactus" class="underline text-gray-300 font-medium">Questions?Contact us.</a>
+
+                <span class="flex gap-16 md:gap-48 pt-5 items-center pb-10">
+                    <div class="flex flex-col underline text-gray-300 space-y-4">
+                        <a href="https://help.netflix.com/support/412">FAQ</a>
+                        <a href="https://media.netflix.com/">Media Center</a>
+                        <a href="https://www.netflix.com/watch">Ways to Watch</a>
+                        <a href="https://www.netflix.com/ng/#">Cookie Preferences</a>
+                        <a href="https://fast.com/">Speed Test</a>
+                    </div>
+
+                    <div class="flex flex-col underline text-gray-300 space-y-4 ">
+                        <a href="https://help.netflix.com/">Help Center</a>
+                        <a href="http://ir.netflix.com/">Investor Relations</a>
+                        <a href="https://help.netflix.com/legal/termsofuse">Terms of Use</a>
+                        <a href="https://help.netflix.com/legal/corpinfo">Corporate Information</a>
+                        <a href="https://help.netflix.com/legal/notices/">Legal Notices</a>
+                    </div>
+
+                    <div class="flex flex-col underline text-gray-300 space-y-4">
+                        <a href="https://www.netflix.com/youraccount">Account</a>
+                        <a href="https://jobs.netflix.com/jobs">Jobs</a>
+                        <a href="https://help.netflix.com/legal/privacy">Privacy</a>
+                        <a href="https://help.netflix.com/contactus">Contact Us</a>
+                        <a href="https://www.netflix.com/ng/browse/genre/839338">Only on Netflix</a>
+                    </div>                   
+                </span>
+
+                <button class=" border border-gray-300 rounded px-3"><i class="fa fa-globe" aria-hidden="true"></i> English <i class="fa-solid fa-caret-down"></i></button>
+
+                <p class="text-gray-300 pt-5">Netflix Nigeria</p>
+            </div>
+
+
         </div>            
     </div>
 
